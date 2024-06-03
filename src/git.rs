6472a17ed6308a0457430e01069e70ebff99f91b
Qi <56908202+qi-xmu@users.noreply.github.com>
2024-06-03 21:31:52 +0800

    feat(Cargo.lock,Cargo.toml,src/git.rs,src/main.rs): Add git2 dependency and related git functions

---
+++ src/git.rs
@@ -0,0 +1,66 @@
+use anyhow::Result;
+use git2::Repository;
+pub fn git_commit(git_base: &str, message: &str) -> Result<()> {
+    let repo = Repository::open(git_base)?;
+    let mut index = repo.index()?;
+
+    let sig = repo.signature()?;
+
+    let tree_id = index.write_tree()?;
+    let tree = repo.find_tree(tree_id)?;
+    println!("Tree: {:?}", tree);
+    println!("Tree id: {:?}", tree_id);
+
+    let parent_commit = repo.head()?.resolve()?.peel_to_commit()?;
+
+    println!("Parent commit: {:?}", parent_commit);
+
+    repo.commit(Some("HEAD"), &sig, &sig, message, &tree, &[&parent_commit])?;
+
+    println!("Commit success");
+    Ok(())
+}
+
+pub fn read_git_diff() -> Result<String> {
+    let status = std::process::Command::new("git")
+        .arg("status")
+        .output()
+        .expect("failed to execute process");
+
+    let diff = std::process::Command::new("git")
+        .arg("diff")
+        .arg("--cached")
+        .output()
+        .expect("failed to execute process");
+
+    let status = String::from_utf8_lossy(&status.stdout);
+    let diff = String::from_utf8_lossy(&diff.stdout);
+
+    let res = format!("{}\n{}", status, diff);
+    Ok(res)
+}
+
+pub fn get_git_base_path() -> Result<String> {
+    let output = std::process::Command::new("git")
+        .arg("rev-parse")
+        .arg("--show-toplevel")
+        .output()?;
+
+    let basepath = String::from_utf8_lossy(&output.stdout);
+    let path_str = basepath.trim().to_string();
+
+    let path = std::path::Path::new(&path_str);
+
+    if path.exists() {
+        Ok(path_str)
+    } else {
+        Err(anyhow::anyhow!("Path not exists"))
+    }
+}
+
+#[test]
+fn test_git_commit() {
+    let git_base = get_git_base_path().unwrap();
+    let message = "test commit";
+    git_commit(&git_base, message).unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,28 @@
 mod config;
 mod coze;
+mod git;
 
-use anyhow::Result;
 use clap::Parser;
-use tokio;
 
 #[derive(Parser)]
 struct Args {}
-
-fn read_git_diff() -> Result<String> {
-    let status = std::process::Command::new("git")
-        .arg("status")
-        .output()
-        .expect("failed to execute process");
-
-    let diff = std::process::Command::new("git")
-        .arg("diff")
-        .arg("--cached")
-        .output()
-        .expect("failed to execute process");
-
-    let status = String::from_utf8_lossy(&status.stdout);
-    let diff = String::from_utf8_lossy(&diff.stdout);
-
-    let res = format!("{}\n{}", status, diff);
-    Ok(res)
-}
 
 #[tokio::main]
 async fn main() {
     let _args = Args::parse();
 
     if let Some(config) = config::read_config() {
-        let diff = read_git_diff().unwrap();
+        let diff = git::read_git_diff().unwrap();
         let resp = coze::request_bot(&config.bot_id, &config.token, &diff).await;
         if let Ok(resp) = resp {
             let message = coze::parse_commit_message(resp).await.unwrap();
             println!("{}", message);
+
+            // commit
+            if let Ok(git_base) = git::get_git_base_path() {
+                println!("Git base path: {}", git_base);
+                git::git_commit(&git_base, &message).unwrap();
+            }
         }
     } else {
         println!("Please set your config file first.");
