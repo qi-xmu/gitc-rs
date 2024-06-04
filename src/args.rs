70affe7f108ee05274a6f051b14914b7602615e3
Qi <56908202+qi-xmu@users.noreply.github.com>
2024-06-04 21:05:54 +0800

    chore(src/args.rs): Minor modifications to Args struct.

--- src/args.rs
+++ src/args.rs
@@ -2,11 +2,11 @@
 pub struct Args {
     /// Confirm or not.
     #[arg(short, long, default_value_t = false)]
-    pub yes: bool, // confirm
+    pub yes: bool,
     /// git add
     #[arg(short, long, default_value_t = false)]
-    pub add_all: bool, // git add
+    pub add_all: bool,
     /// git push
     #[arg(short, long, default_value_t = false)]
-    pub push: bool, // git push
+    pub push: bool,
 }
