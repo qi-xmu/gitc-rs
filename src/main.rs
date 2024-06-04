61b342705ccec7222371a3043752fd611cf64b51
Qi <56908202+qi-xmu@users.noreply.github.com>
2024-06-04 18:56:39 +0800

    refactor(src/main.rs): Modified code for config reading and git operations handling

--- src/main.rs
+++ src/main.rs
@@ -10,32 +10,36 @@ struct Args {}
 #[tokio::main]
 async fn main() {
     let _args = Args::parse();
-
-    if let Some(config) = config::read_config() {
-        // git exist
-        if let Ok(git_base) = git::get_git_base_path() {
-            let repo = git::get_reop(&git_base).unwrap();
-            // git has changes
-            if git::commit_or_not(&repo).is_ok() {
-                let diff = git::read_git_diff().unwrap();
-                // request bot for commit message
-                let resp = coze::request_bot(&config.bot_id, &config.token, &diff)
-                    .await
-                    .expect("Request bot failed.");
-                if let Ok(message) = coze::parse_commit_message(resp).await {
+    // read config
+    let config =
+        config::read_config().expect("Please set your config file first. Default path: ~/.gitc");
+    // git exist
+    let git_base = git::get_git_base_path().expect("Git project not found.");
+    let repo = git::get_reop(&git_base).expect("Git repo not found.");
+    // git has changes
+    if git::commit_or_not(&repo).is_ok() {
+        let diff = git::read_git_diff().unwrap();
+        // request bot for commit message
+        let resp = coze::request_bot(&config.bot_id, &config.token, &diff)
+            .await
+            .expect("Request bot failed.");
+        if let Ok(message) = coze::parse_commit_message(resp).await {
+            // 询问是否提交
+            loop {
+                let mut input = String::new();
+                println!("Commit message: {}", message);
+                println!("Do you want to commit? (y/n)");
+                std::io::stdin().read_line(&mut input).unwrap();
+                if input.trim() == "y" {
                     git::git_commit(&repo, &message).unwrap();
                     println!("Commit message: {}", message);
-                } else {
-                    println!("Empty message. Maybe config is wrong.");
+                    break;
                 }
-            } else {
-                println!("No changes to commit.");
             }
         } else {
-            println!("Git project not found.");
+            println!("Empty message. Maybe config is wrong.");
         }
     } else {
-        println!("Please set your config file first.");
-        println!("Default path: ~/.gitc");
+        println!("No changes to commit.");
     }
 }
