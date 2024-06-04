b40abd0df3078009df8c95e6c941e6d140a87bbd
Qi <56908202+qi-xmu@users.noreply.github.com>
2024-06-04 20:13:03 +0800

    feat(src/args.rs): Added yes, add_all and push fields to Args struct

--- src/args.rs
+++ src/args.rs
@@ -1,9 +1,12 @@
 #[derive(clap::Parser)]
 pub struct Args {
+    /// Confirm or not.
     #[arg(short, long, default_value_t = false)]
     pub yes: bool, // confirm
+    /// git add
     #[arg(short, long, default_value_t = false)]
     pub add_all: bool, // git add
+    /// git push
     #[arg(short, long, default_value_t = false)]
     pub push: bool, // git push
 }
